67096d826c80be037dd31264c77f54fa0602461b
SatishPulleti <pulletisatish123@gmail.com>
2023-10-14 11:26:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Home from "./portfolio/Home";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import {HashRouter, Routes, Route} from 'react-router-dom';
+import About from './portfolio/About';
+import Footer from './portfolio/footer';
+import Skill from './portfolio/skill';
+import Projects from './portfolio/project';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HashRouter>
+        {/* <Home/> */}
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/about' element={<About/>} />
+          <Route path='/skill' element={<Skill/>} />
+          <Route path='/projects' element={<Projects/>} />
+        </Routes>
+        <Footer/>
+      </HashRouter>
     </div>
   );
 }
